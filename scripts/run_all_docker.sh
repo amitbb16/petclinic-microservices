6648cff039c46da9551778fe5452ff2d970b7d61
Amit Personal <amitbiradar648@gmail.com>
2023-12-12 01:16:36 +0530

    Compose

--- scripts/run_all_docker.sh
+++ scripts/run_all_docker.sh
@@ -36,6 +36,6 @@ docker run -p 127.0.0.1:8081:8081 -itd --network=petnet --name customers-service
 docker run -p 127.0.0.1:8082:8082 -itd --network=petnet --name visits-service amitbb16/petclinic-visits-service:v1
 docker run -p 127.0.0.1:8083:8083 -itd --network=petnet --name vets-service amitbb16/petclinic-vets-service:v1
 docker run -p 127.0.0.1:8080:8080 -itd --network=petnet --name api-gateway amitbb16/petclinic-api-gateway:v1
-docker run -p 127.0.0.1:9091:9091 -itd --network=petnet --name admin-serve amitbb16/petclinic-admin-server:v3
+docker run -p 127.0.0.1:9091:9091 -itd --network=petnet --name admin-server amitbb16/petclinic-admin-server:v3
 echo "Waiting for apps to start"
 sleep 60
