ff24b5ec5f5f10753e64f6400b6466905eb306a3
Amit Personal <amitbiradar648@gmail.com>
2023-12-12 08:42:00 +0530

    Dockerfile

--- scripts/network.sh
+++ scripts/network.sh
@@ -20,6 +20,6 @@ docker run -p 127.0.0.1:8888:8888 -d --network=petnet --name config-server amitb
 echo "Waiting for config server to start"
 sleep 20
 
-docker run -p 127.0.0.1:8761:8761 -itd --network=petnet --name discovery-server amitbb16/petclinic-discovery-server:v1
+docker run -p 127.0.0.1:8761:8761 -itd --network=petnet --name discovery-server amitbb16/petclinic-discovery-server:v6
 echo "Waiting for discovery server to start"
 sleep 20
